5f22b438b5c57aeb2807d4203e756d1b615d4f88
Leandro-Barral <141652604+Leandro-Barral@users.noreply.github.com>
2023-10-05 23:39:19 -0300

    Update and rename Encounter.cs to IEncounter.cs

--- src/Library/IEncounter.cs
+++ src/Library/IEncounter.cs
@@ -1,8 +1,10 @@
 namespace RoleplayGame;
 
-public interface Encounter
+public interface IEncounter
 {
   public list<Heroe> heroes;
   public list<Enemigo> enemigos;
+  public void AddHero(Heroe heroe);
+  public void AddEnemy(Enemigo enemigo);
   public void DoEncounter();
 }
